dc810128ff257fe3f9fd41c84919da9f616874da
tounektimohamed <87899355+tounektimohamed@users.noreply.github.com>
2025-05-07 10:01:38 +0100

    Update footer-info.js
    test

--- client/src/constants/footer-info.js
+++ client/src/constants/footer-info.js
@@ -1,23 +1,23 @@
 export const FOOTER_INTRO = {
-    copyright: '© Vítor F. Nery'
+    copyright: '© Mohamed Tounekti'
 }
 
 export const FOOTER_LOCATE = {
-    title:"locate us",
-    city:"Curitiba, PR, Brasil",
-    mobile:"Mobile: +55 41 997668680",
-    email:"E-mail: vitorfelipenery@gmail.com",
-    phone:"Phone: +55 41 32345678<"
+    title: "Nous localiser",
+    city: "Tataouine, Tunisie",
+    mobile: "Mobile : +216 21181239 ", // Remplace par ton vrai numéro
+    email: "E-mail : tounekti17@gmail.com", // Remplace par ton vrai email
+    phone: "Téléphone : +216 21181239" // Remplace si différent du mobile
 }
 
 export const FOOTER_PROFILE = {
-    title:"profile",
-    account:"my account",
-    checkout:"checkout",
-    track:"order tracking",
-    help:"help & support"
+    title: "Profil",
+    account: "Mon compte",
+    checkout: "Paiement",
+    track: "Suivi de commande",
+    help: "Aide & support"
 }
 
 export const FOOTER_SUBSCRIBE = {
-    btn: "Subscribe"
+    btn: "S'abonner"
 }
